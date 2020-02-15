59961b3e039367bf61023d51dbe2d9028fb1ea47
popo8 <cop0314@gmail.com>
2020-02-15 10:16:08 +0900

    ユニティプロジェクトの追跡

---
+++ Bom/Assets/script/BomBulletRund.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomBulletRund : MonoBehaviour
+{
+    public Transform myTransform;
+    public GameObject BomBullets;
+    Vector3 bomPos;
+    float span;
+    float delta;
+    int bulletVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myTransform = this.transform;
+        bomPos = myTransform.position;
+        bulletVolume = 50;
+        delta = 0.0f;
+        span = 0.00001f;
+    }
+
+    // Update is called once per frame
+    //void BulletRand()
+    //{
+    //    this.delta += Time.deltaTime;
+    //    if (this.delta > this.span && bulletVolume > 0)
+    //    {
+    //        bulletVolume--;
+    //        this.delta = 0;
+    //        GameObject itemBomBullets = Instantiate(BomBullets) as GameObject;
+    //        float x = bomPos.x;
+    //        float y = bomPos.y;
+    //        float z = bomPos.z;
+    //        itemBomBullets.transform.position = new Vector3(x, y, z);
+    //        float ry = Random.Range(0.0f, 360.0f);
+    //        itemBomBullets.transform.Rotate(0.0f, ry, 0.0f);
+
+
+
+    //    }
+
+    //}
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span && bulletVolume > 0)
+        {
+            bulletVolume--;
+            this.delta = 0;
+            GameObject itemBomBullets = Instantiate(BomBullets) as GameObject;
+            float x = bomPos.x;
+            float y = bomPos.y;
+            float z = bomPos.z;
+            itemBomBullets.transform.position = new Vector3(x, y, z);
+            float ry = Random.Range(0.0f, -720.0f);
+            itemBomBullets.transform.Rotate(0.0f, ry, 0.0f);
+
+
+
+        }
+    }
+}
---
+++ Bom/Assets/script/BomBullets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomBullets : MonoBehaviour
+{
+    float delta;
+    public float advanceBullet;
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        delta = 0.0f;
+        advanceBullet = -0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        /*transform.Translate*/
+        rb.AddForce(/*this.advanceBullet,0,0*/1,1,1);
+        if (delta >= 2.0f)
+        {
+            delta = 0.0f;
+            Destroy(gameObject);
+        }
+    }
+}
